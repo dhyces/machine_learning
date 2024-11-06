59e7bf8d603255a566878c6121ce65cafc438806
dhyces <10985914+dhyces@users.noreply.github.com>
2024-11-05 23:33:50 -0500

    Fix optimization and performance improvement
    There were difficulties with Tensor::stack and using so many tensors and cloning them was slowing things down. It's rather quick now with CUDA cores

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 mod translator;
 
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Read};
-use std::ops::{Deref, Range};
+use std::ops::{Deref};
 use candle_core::{Module, DType, Device, IndexOp, Tensor};
 use candle_nn::{embedding, Embedding, Optimizer, VarBuilder, VarMap};
 use itertools::Itertools;
 use rand::distributions::Distribution;
 use rand::rngs::StdRng;
-use rand::SeedableRng;
-use crate::translator::{Translator, U32ToChar};
+use rand::{Rng, SeedableRng};
+use crate::translator::{CharToU32, Translator, U32ToChar};
 
-fn main() {
+const RAND_SEED: u64 = 1337;
+
+fn main() -> candle_core::Result<()> {
     // Read file and collect all unique tokens, sorted
     let mut file = BufReader::new(File::open(r"C:\Users\pokmo\OneDrive\Desktop\DevStuff\Rust\deep_learning\input.txt").expect("Should be a valid file"));
     let mut contents = String::new();
@@ -24,71 +25,67 @@ fn main() {
 
     let device = Device::cuda_if_available(0).expect("Should be able to leverage CUDA cores");
 
-    let data = Tensor::new(contents.as_bytes(), &device).expect("Should be able to construct tensor");
+    let data = translator.char_array_to_u32s(&contents.chars().collect::<Vec<char>>()).unwrap();
     // print!("{}", data.i(..1000).unwrap());
 
     // Setup to use the first 90% for training data and the remaining 10% for validation
-    let n = (0.9*data.dim(0).unwrap() as f32) as usize;
-    let train_data = data.i(..n).unwrap();
-    let validation_data = data.i(n..).unwrap();
+    let n = (data.len() * 9) / 10;
+    let (train_data, _validation_data) = data.split_at(n);
+
+    let mut rng = StdRng::seed_from_u64(RAND_SEED);
 
     let batch_size = 4;
     let block_size = 8;
 
     // computationally expensive to train on the full dataset, so just take chunks from it.
-    let (xb, yb) = get_batch(&train_data, batch_size, block_size);
+    let (xb, yb) = get_batch(&train_data, &device, batch_size, block_size, &mut rng)?;
     println!("{}", xb);
     println!("{}", yb);
 
     let var_map = VarMap::new();
     let var_builder = VarBuilder::from_varmap(&var_map, DType::F32, &device);
-    let mut model = BigramLanguageModel::new(vocab_size, var_builder);
-    let (logits, loss) = ModuleWithLoss::forward(&model, &xb, &yb).unwrap();
+    let mut model = BigramLanguageModel::new(vocab_size, var_builder, rng.clone());
+    let (_, loss) = ModuleWithLoss::forward(&model, &xb, &yb)?;
     println!("{}", loss);
-    let generated = model.generate(&Tensor::zeros((1, 1), DType::U32, &device).unwrap(), 100).unwrap();
-    println!("{:?}", translator.u32_array_to_chars(&generated.to_vec2::<u32>().unwrap()[0]));
+    let generated = model.generate(&Tensor::zeros((1, 1), DType::U32, &device)?, 100)?;
+    println!("{:?}", translator.u32_array_to_chars(&generated.to_vec2::<u32>()?[0]));
 
     let map_vars = var_map.all_vars();
     println!("{:?}", &map_vars);
-    let mut optimizer = candle_nn::optim::AdamW::new_lr(map_vars, 1e-3).unwrap();
+    let mut optimizer = candle_nn::optim::AdamW::new_lr(map_vars, 1e-3)?;
 
     let mut final_loss = None;
-    for steps in 0..12000 {
+    for steps in 0..6000 {
         if steps % 300 == 0 {
-            let (xn, yn) = get_batch(&train_data, 32, block_size);
-            let (_, loss) = ModuleWithLoss::forward(&model, &xn, &yn).unwrap();
-            println!("Loss: {}", loss);
+            let (xn, yn) = get_batch(&train_data, &device, 32, block_size, &mut rng)?;
+            let (_, loss) = ModuleWithLoss::forward(&model, &xn, &yn)?;
+            println!("Iteration: {} Loss: {}", steps, loss);
         }
-        let (xn, yn) = get_batch(&train_data, 32, block_size);
+        let (xn, yn) = get_batch(&train_data, &device, 32, block_size, &mut rng)?;
 
-        let (_, loss) = ModuleWithLoss::forward(&model, &xn, &yn).unwrap();
-        optimizer.backward_step(&loss).unwrap();
+        let (_, loss) = ModuleWithLoss::forward(&model, &xn, &yn)?;
+        optimizer.backward_step(&loss)?;
         final_loss = Some(loss);
     }
     println!("{}", final_loss.unwrap());
 
-    let generated = model.generate(&Tensor::zeros((1, 1), DType::U32, &device).unwrap(), 100).unwrap();
-    println!("{:?}", translator.u32_array_to_chars(&generated.to_vec2::<u32>().unwrap()[0]));
+    let generated = model.generate(&Tensor::zeros((1, 1), DType::U32, &device)?, 100)?;
+    println!("{}", translator.u32_array_to_chars(&generated.to_vec2::<u32>()?[0]).unwrap().iter().collect::<String>());
+    Ok(())
 }
 
-fn get_batch(tensor: &Tensor, batch_size: usize, block_size: usize) -> (Tensor, Tensor) {
-    // generate random locations within data to use as block starting locations
-    let ix = Tensor::rand(0.0, (tensor.dims1().unwrap() - block_size) as f32, (batch_size,), tensor.device())
-        .and_then(|t| t.to_dtype(DType::U32)).unwrap();
+fn get_batch(data: &[u32], device: &Device, batch_size: usize, block_size: usize, rng: &mut StdRng) -> candle_core::Result<(Tensor, Tensor)> {
     // builds the input values. Each set of these are associated with an index of the target
     // values, which serves as the "correct" answer to that set of inputs.
-    let x = stack_range(tensor, ix.to_vec1().unwrap(), |i| i..i + block_size).unwrap();
-    let y = stack_range(tensor, ix.to_vec1().unwrap(), |i| i + 1.. i + block_size + 1).unwrap();
-    // println!("x: {:?}, y: {:?}", x.shape(), y.shape());
-    (x, y)
-}
-
-fn stack_range<F: Fn(usize) -> Range<usize>>(tensor: &Tensor, indices: Vec<u32>, range: F) -> candle_core::Result<Tensor> {
-    let mut vec = Vec::new();
-    for i in indices {
-        vec.push(tensor.i(range(i as usize))?);
+    let mut xx = vec![vec![0u32; block_size]; batch_size];
+    let mut yy = vec![vec![0u32; block_size]; batch_size];
+    for (_, (x, y)) in xx.iter_mut().zip(yy.iter_mut()).enumerate() {
+        // generate random location within data to use as block starting location
+        let start_val = rng.gen_range(0..batch_size + block_size);
+        x.copy_from_slice(&data[start_val..start_val+block_size]);
+        y.copy_from_slice(&data[start_val+1..start_val+block_size+1]);
     }
-    Tensor::stack(vec.deref(), tensor.dims().len())
+    Ok((Tensor::new(xx, device)?, Tensor::new(yy, device)?))
 }
 
 trait ModuleWithLoss {
@@ -101,12 +98,12 @@ struct BigramLanguageModel {
 }
 
 impl BigramLanguageModel {
-    pub fn new(vocab_size: usize, var_builder: VarBuilder) -> Self {
+    pub fn new(vocab_size: usize, var_builder: VarBuilder, rng: StdRng) -> Self {
         Self {
             // light wrapper over a tensor, meant for retrieving rows
             // NOTE: This cannot be of DType::I64 due to a lack of support for uexp_ for 64-bit signed integers in CUDA
             token_embedding_table: embedding(vocab_size, vocab_size, var_builder).unwrap(),
-            rng: StdRng::seed_from_u64(1337u64),
+            rng
         }
     }
 
@@ -134,10 +131,10 @@ impl Module for BigramLanguageModel {
 impl ModuleWithLoss for BigramLanguageModel {
     fn forward(&self, i: &Tensor, xs: &Tensor) -> candle_core::Result<(Tensor, Tensor)> {
         // returns "logits", which allow for prediction from a single token, rather than a set of tokens
-        let mut logits = i.apply(&self.token_embedding_table)?;
+        let logits = i.apply(&self.token_embedding_table)?;
         let (b, t, c) = logits.dims3()?;
         let logits = logits.reshape((b*t, c))?;
-        let targets = xs.reshape((b*t))?;
+        let targets = xs.reshape(b*t)?;
         // in this case, we expect loss to be -ln(1/65), so negative natlog of the inverse vocab size
         let loss = candle_nn::loss::cross_entropy(&logits, &targets)?;
         Ok((logits, loss))
